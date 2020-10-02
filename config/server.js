c3a7b07085a3f54758ad90828cc3e60aa554b2c1
Luiz Otavio <luizomf@gmail.com>
2020-10-02 02:35:21 -0300

    Initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '73b8147c3f16942d61dec3fc44c7b133'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47c539fe-cd63-43fc-91e7-32f9ccdb6a49'
+};
