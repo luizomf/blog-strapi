2372c5a5678c6182c09a5915df3e745139bae787
Luiz Otavio <luizomf@gmail.com>
2020-10-06 04:54:33 -0300

    Removido webhook da netlify dos lifecycles

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -8,13 +8,13 @@ const axios = require('axios');
  */
 
 module.exports = {
-  lifecycles: {
-    async afterCreate(result, data) {
-      axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
-    },
+  // lifecycles: {
+  //   async afterCreate(result, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
+  //   },
 
-    async afterUpdate(result, params, data) {
-      axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
-    },
-  }
+  //   async afterUpdate(result, params, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
+  //   },
+  // }
 };
